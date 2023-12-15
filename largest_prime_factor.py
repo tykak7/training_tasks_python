7cca4d8f3474cd5a3f36c121e07e5f1c8d4fe2ba
tykak7 <150191653+tykak7@users.noreply.github.com>
2023-12-15 20:49:23 +0600

    Translate to ENG

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,12 +1,15 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# Find the greatest prime divisor of 70616204741131.
+
 n = 70616204741131
-p = 3 # kontrolujeme dělitele
+p = 3 # checking the divisors
 
 while p * p < n:
-    if n % p == 0: # test pro dělení beze zbytku
-        n //= p # pokud dělí beze zbytku, je výsledek
-        # dělení přiřazen proměnné n
+    if n % p == 0: # test for division without remainder
+        n //= p # if it divides without remainder, the result is
+        # division assigned to the variable n
 
     else:
-        p += 2 # zvětšíme dělitele, liché číslo
+        p += 2 # increase the divisor, odd number
    
 print(n) 
